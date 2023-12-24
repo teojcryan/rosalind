ad38ad1a901140a05d41384fc4a700c5b47c1cd2
teojcryan <u2128816@live.warwick.ac.uk>
2023-12-24 20:59:53 +0000

    update utils

--- code/utils.py
+++ code/utils.py
@@ -3,22 +3,20 @@ class Graph:
         self.nodes = nodes if nodes else []
         self.edges = edges if edges else {}
 
-    def add_edge(self, u, v):
-        # For undirected graphs
-        if u not in self.edges:
-            self.edges[u] = []
-        self.edges[u].append(v)
-        if v not in self.edges:
-            self.edges[v] = []
-        self.edges[v].append(u)
-        return self
-
-    def add_directed_edge(self, u, v):
-        # For directed graphs
-        if u not in self.edges:
-            self.edges[u] = []
-        self.edges[u].append(v)
-        return self
+    def add_edge(self, u, v, directed):
+        if directed:
+            if u not in self.edges:
+                self.edges[u] = []
+            self.edges[u].append(v)
+            return self
+        else:
+            if u not in self.edges:
+                self.edges[u] = []
+            self.edges[u].append(v)
+            if v not in self.edges:
+                self.edges[v] = []
+            self.edges[v].append(u)
+            return self
 
     def generate_from_edge_list_file(self, file_path, directed=False):
         with open(file_path, 'r') as file:
@@ -27,8 +25,5 @@ class Graph:
             self.nodes = list(range(1, n + 1))
             for line in lines[1:]:
                 u, v = map(int, line.split())
-                if directed:
-                    self.add_directed_edge(u, v)
-                else:
-                    self.add_edge(u, v)
+                self.add_edge(u, v, directed)
         return self
